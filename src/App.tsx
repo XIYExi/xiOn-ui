b3ad6bf2e6c219ea7f3434f9ceadbd8c60009932
xiye <Zhou730428@outlook.com>
2022-03-17 20:55:39 +0800

    这是一个测试

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App:React.FC = () => {
 
     return(
         <>
-            {/*<Switch onChange={(e)=>console.log(e)}/>*/}
+            <Switch onChange={(e)=>console.log(e)}/>
 
         </>
     )
